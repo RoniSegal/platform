76993a07632cd915d3b22c9889d1f689e5fc8dc4
sealightsci <sealightsci@gmail.com>
2017-01-29 14:30:09 +0200

    Update EventTypeMetadata.java

--- event/src/main/java/com/proofpoint/event/client/EventTypeMetadata.java
+++ event/src/main/java/com/proofpoint/event/client/EventTypeMetadata.java
@@ -41,10 +41,8 @@ import static com.proofpoint.event.client.EventDataType.getEventDataType;
 import static com.proofpoint.event.client.EventFieldMetadata.ContainerType;
 import static com.proofpoint.configuration.TypeParameterUtils.getTypeParameters;
 
-final class EventTypeMetadata<T>
-{
-    public static Set<EventTypeMetadata<?>> getValidEventTypeMetaDataSet(Class<?>... eventClasses)
-    {
+final class EventTypeMetadata<T> {
+    public static Set<EventTypeMetadata<?>> getValidEventTypeMetaDataSet(Class<?>... eventClasses) {
         ImmutableSet.Builder<EventTypeMetadata<?>> set = ImmutableSet.builder();
         for (Class<?> eventClass : eventClasses) {
             set.add(getValidEventTypeMetadata(eventClass));
@@ -52,8 +50,7 @@ final class EventTypeMetadata<T>
         return set.build();
     }
 
-    public static <T> EventTypeMetadata<T> getValidEventTypeMetadata(Class<T> eventClass)
-    {
+    public static <T> EventTypeMetadata<T> getValidEventTypeMetadata(Class<T> eventClass) {
         EventTypeMetadata<T> metadata = getEventTypeMetadata(eventClass);
         if (!metadata.getErrors().isEmpty()) {
             String errors = Joiner.on('\n').join(metadata.getErrors());
@@ -62,8 +59,7 @@ final class EventTypeMetadata<T>
         return metadata;
     }
 
-    public static <T> EventTypeMetadata<T> getEventTypeMetadata(Class<T> eventClass)
-    {
+    public static <T> EventTypeMetadata<T> getEventTypeMetadata(Class<T> eventClass) {
         return new EventTypeMetadata<>(eventClass, Lists.<String>newArrayList(), Maps.<Class<?>, EventTypeMetadata<?>>newHashMap(), false);
     }
 
@@ -76,8 +72,7 @@ final class EventTypeMetadata<T>
     private final List<EventFieldMetadata> fields;
     private final List<String> errors;
 
-    private EventTypeMetadata(Class<T> eventClass, List<String> errors, Map<Class<?>, EventTypeMetadata<?>> metadataClasses, boolean nestedEvent)
-    {
+    private EventTypeMetadata(Class<T> eventClass, List<String> errors, Map<Class<?>, EventTypeMetadata<?>> metadataClasses, boolean nestedEvent) {
         Preconditions.checkNotNull(eventClass, "eventClass is null");
         Preconditions.checkNotNull(errors, "errors is null");
         Preconditions.checkNotNull(metadataClasses, "metadataClasses is null");
@@ -114,12 +109,10 @@ final class EventTypeMetadata<T>
             if (isIterable(dataType)) {
                 dataType = extractIterableType(method);
                 containerType = ContainerType.ITERABLE;
-            }
-            else if (isMap(dataType)) {
+            } else if (isMap(dataType)) {
                 dataType = extractMapType(method, Map.class);
                 containerType = ContainerType.MAP;
-            }
-            else if (isMultimap(dataType)) {
+            } else if (isMultimap(dataType)) {
                 dataType = extractMapType(method, Multimap.class);
                 containerType = ContainerType.MULTIMAP;
             }
@@ -133,8 +126,7 @@ final class EventTypeMetadata<T>
 
             if (isNestedEvent(dataType)) {
                 nestedType = getNestedEventTypeMetadata(dataType, metadataClasses);
-            }
-            else {
+            } else {
                 eventDataType = getEventDataType(dataType);
                 if (eventDataType == null) {
                     Object typeSource = (containerType != null) ? containerType : "return";
@@ -161,8 +153,7 @@ final class EventTypeMetadata<T>
                     continue;
                 }
                 fieldName = eventField.fieldMapping().getFieldName();
-            }
-            else {
+            } else {
                 if (fieldName.isEmpty()) {
                     fieldName = extractNameFromGetter(method);
                 }
@@ -179,8 +170,7 @@ final class EventTypeMetadata<T>
             EventFieldMetadata eventFieldMetadata = new EventFieldMetadata(fieldName, method, eventDataType, nestedType, containerType);
             if (eventField.fieldMapping() == EventFieldMapping.DATA) {
                 fields.put(fieldName, eventFieldMetadata);
-            }
-            else {
+            } else {
                 specialFields.put(eventField.fieldMapping(), eventFieldMetadata);
             }
         }
@@ -205,8 +195,7 @@ final class EventTypeMetadata<T>
         }
     }
 
-    private String extractTypeName(Class<T> eventClass, boolean nestedEvent)
-    {
+    private String extractTypeName(Class<T> eventClass, boolean nestedEvent) {
         EventType typeAnnotation = eventClass.getAnnotation(EventType.class);
         if (typeAnnotation == null) {
             addClassError("is not annotated with @%s", EventType.class.getSimpleName());
@@ -217,18 +206,15 @@ final class EventTypeMetadata<T>
             if (!typeName.isEmpty()) {
                 addClassError("specifies an event name but is used as a nested event");
             }
-        }
-        else if (typeName.isEmpty()) {
+        } else if (typeName.isEmpty()) {
             addClassError("does not specify an event name");
-        }
-        else if (!isValidEventName(typeName)) {
+        } else if (!isValidEventName(typeName)) {
             addClassError("Event name is invalid [%s]", typeName);
         }
         return typeName;
     }
 
-    private Class<?> extractIterableType(Method method)
-    {
+    private Class<?> extractIterableType(Method method) {
         Type[] types = getTypeParameters(Iterable.class, method.getGenericReturnType());
         if ((types == null) || (types.length != 1)) {
             addMethodError("Unable to get type parameter for iterable [%s]", method, method.getGenericReturnType());
@@ -246,8 +232,7 @@ final class EventTypeMetadata<T>
         return (Class<?>) type;
     }
 
-    private Class<?> extractMapType(Method method, Class<?> mapClass)
-    {
+    private Class<?> extractMapType(Method method, Class<?> mapClass) {
         String className = mapClass.getSimpleName();
         Type[] types = getTypeParameters(mapClass, method.getGenericReturnType());
         if ((types == null) || (types.length != 2)) {
@@ -275,8 +260,7 @@ final class EventTypeMetadata<T>
     }
 
     @SuppressWarnings("unchecked")
-    private EventTypeMetadata<?> getNestedEventTypeMetadata(Class<?> eventClass, Map<Class<?>, EventTypeMetadata<?>> metadataClasses)
-    {
+    private EventTypeMetadata<?> getNestedEventTypeMetadata(Class<?> eventClass, Map<Class<?>, EventTypeMetadata<?>> metadataClasses) {
         EventTypeMetadata<?> metadata = metadataClasses.get(eventClass);
         if (metadata != null) {
             return metadata;
@@ -286,8 +270,7 @@ final class EventTypeMetadata<T>
         return new EventTypeMetadata(eventClass, errors, metadataClasses, true);
     }
 
-    private void findInvalidMethods(Class<T> eventClass)
-    {
+    private void findInvalidMethods(Class<T> eventClass) {
         // find invalid methods that were skipped by findAnnotatedMethods()
         for (Class<?> clazz = eventClass; clazz != null; clazz = clazz.getSuperclass()) {
             for (Method method : clazz.getDeclaredMethods()) {
@@ -303,8 +286,7 @@ final class EventTypeMetadata<T>
         }
     }
 
-    private static String extractNameFromGetter(Method method)
-    {
+    private static String extractNameFromGetter(Method method) {
         String name = method.getName();
         if (name.length() > 3 && name.startsWith("get")) {
             return lowerCaseFirstCharacter(name.substring(3));
@@ -315,94 +297,76 @@ final class EventTypeMetadata<T>
         return name;
     }
 
-    private static String lowerCaseFirstCharacter(String s)
-    {
+    private static String lowerCaseFirstCharacter(String s) {
         return s.substring(0, 1).toLowerCase() + s.substring(1);
     }
 
-    private static boolean isString(Class<?> type)
-    {
+    private static boolean isString(Class<?> type) {
         return String.class.isAssignableFrom(type);
     }
 
-    private static boolean isIterable(Class<?> type)
-    {
+    private static boolean isIterable(Class<?> type) {
         return Iterable.class.isAssignableFrom(type);
     }
 
-    private static boolean isMap(Class<?> type)
-    {
+    private static boolean isMap(Class<?> type) {
         return Map.class.isAssignableFrom(type);
     }
 
-    private static boolean isMultimap(Class<?> type)
-    {
+    private static boolean isMultimap(Class<?> type) {
         return Multimap.class.isAssignableFrom(type);
     }
 
-    private static boolean isNestedEvent(Class<?> type)
-    {
+    private static boolean isNestedEvent(Class<?> type) {
         return type.isAnnotationPresent(EventType.class);
     }
 
-    private static boolean isValidFieldName(String name)
-    {
+    private static boolean isValidFieldName(String name) {
         return name.matches("[a-z][A-Za-z0-9]*");
     }
 
-    private static boolean isValidEventName(String name)
-    {
+    private static boolean isValidEventName(String name) {
         return name.matches("[A-Za-z0-9.:=,-]*");
     }
 
-    List<String> getErrors()
-    {
+    List<String> getErrors() {
         return ImmutableList.copyOf(errors);
     }
 
-    public Class<T> getEventClass()
-    {
+    public Class<T> getEventClass() {
         return eventClass;
     }
 
-    public String getTypeName()
-    {
+    public String getTypeName() {
         return typeName;
     }
 
-    public EventFieldMetadata getUuidField()
-    {
+    public EventFieldMetadata getUuidField() {
         return uuidField;
     }
 
-    public EventFieldMetadata getTimestampField()
-    {
+    public EventFieldMetadata getTimestampField() {
         return timestampField;
     }
 
-    public EventFieldMetadata getHostField()
-    {
+    public EventFieldMetadata getHostField() {
         return hostField;
     }
 
-    public EventFieldMetadata getTraceTokenField()
-    {
+    public EventFieldMetadata getTraceTokenField() {
         return traceTokenField;
     }
 
-    public List<EventFieldMetadata> getFields()
-    {
+    public List<EventFieldMetadata> getFields() {
         return fields;
     }
 
-    public void addMethodError(String format, Method method, Object... args)
-    {
+    public void addMethodError(String format, Method method, Object... args) {
         String prefix = String.format("@X method [%s] ", method.toGenericString());
         addClassError(prefix + format, args);
     }
 
-    public void addClassError(String format, Object... args)
-    {
+    public void addClassError(String format, Object... args) {
         String message = String.format(format, args);
         message = String.format("Event class [%s] %s", eventClass, message);
         message = message.replace("@X", EventField.class.getSimpleName());
@@ -411,10 +375,13 @@ final class EventTypeMetadata<T>
 
     @SuppressWarnings("RedundantIfStatement")
     @Override
-    public boolean equals(Object o)
-    {
-            System.out.println("SHAHAR-SHAHAR SHAHAR-SHAHAR SHAHAR-SHAHAR");
-            throw new RuntimeException();
+    public boolean equals(Object o) {
+        System.out.println("SHAHAR-SHAHAR SHAHAR-SHAHAR SHAHAR-SHAHAR");
+        StackTraceElement[] s = Thread.currentThread().getStackTrace();
+        for (StackTraceElement e : s) {
+            System.out.println(e);
+        }
+        System.out.println("SHAHAR-SHAHAR SHAHAR-SHAHAR SHAHAR-SHAHAR");
 //         if (this == o) {
 //             return true;
 //         }
@@ -432,8 +399,7 @@ final class EventTypeMetadata<T>
     }
 
     @Override
-    public int hashCode()
-    {
+    public int hashCode() {
         return eventClass != null ? eventClass.hashCode() : 0;
     }
 }
